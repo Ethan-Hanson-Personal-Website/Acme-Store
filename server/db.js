0d05e6562fee25aa9cb4db65e76988cd4aa43c15
Alivar <ethanhanson9@gmail.com>
2024-03-03 19:38:54 -0800

    update

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_store_db');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost:5432/acme_store_db');
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
 
